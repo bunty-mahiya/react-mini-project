9b69c50e996eaff468c0f5f859ed04043887787c
bunty <buntymahiya764@gamil.com>
2025-09-10 19:51:36 +0530

    create array for shimmer

--- src/Shimmer.jsx
+++ src/Shimmer.jsx
@@ -1,126 +1,18 @@
 export default function ShimmerProductCard() {
   return (
     <div className="shimmer-container">
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
-      <div className="shimmer-product-card">
-        <div className="shimmer-product-image"></div>
-        <div className="shimmer-product-content">
-          <div className="shimmer-line title"></div>
-          <div className="shimmer-line price"></div>
-          <div className="shimmer-line small"></div>
-        </div>
-      </div>
+      {Array(30)
+        .fill("")
+        .map((e,index) => {
+         return <div  key={index}className="shimmer-product-card">
+            <div className="shimmer-product-image"></div>
+            <div className="shimmer-product-content">
+              <div className="shimmer-line title"></div>
+              <div className="shimmer-line price"></div>
+              <div className="shimmer-line small"></div>
+            </div>
+          </div>;
+        })}
     </div>
   );
 }
